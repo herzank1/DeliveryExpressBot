f8c3104bbf46b5ee9c4f010377f6e965c969890c
DeliveryExpress <DeliveryExpress@PC-Old-W10pro>
2024-11-24 10:47:28 -0800

    Delivery Express Bot Omrlite

--- src/main/java/com/deliveryexpress/quizes/QuizNewBussines.java
+++ src/main/java/com/deliveryexpress/quizes/QuizNewBussines.java
@@ -4,14 +4,17 @@
  */
 package com.deliveryexpress.quizes;
 
-import com.deliveryexpress.de.DataBase;
-import com.deliveryexpress.de.OperationsMagnament;
-import com.deliveryexpress.objects.OperationAreaMagnament;
-import com.deliveryexpress.objects.TelegramUser;
-import com.deliveryexpress.objects.TelegramUser.Bussines;
-import com.deliveryexpress.telegram.Messenger;
-import com.deliveryexpress.telegram.Messenger.MessageMenu;
-import com.deliveryexpress.telegram.Messenger.MessageMenu.Button;
+import com.deliveryexpress.de.contability.BalanceAccount;
+import com.deliveryexpress.de.database.DataBase;
+import com.deliveryexpress.objects.users.AccountStatus;
+import com.deliveryexpress.objects.users.AccountType;
+import com.deliveryexpress.objects.users.Bussines;
+import com.deliveryexpress.objects.users.TelegramUser;
+
+import com.deliveryexpress.telegram.MessageMenu;
+import com.deliveryexpress.telegram.Methods;
+import com.deliveryexpress.telegram.Response;
+
 import com.deliveryexpress.telegram.Xupdate;
 import com.deliveryexpress.utils.Utils;
 import java.util.ArrayList;
@@ -22,7 +25,6 @@ import java.util.ArrayList;
  */
 public class QuizNewBussines extends Quiz {
 
-    String telegramId;
     String name, phone, address, area;
 
     Bussines bussines;
@@ -34,133 +36,104 @@ public class QuizNewBussines extends Quiz {
     @Override
     void execute(Xupdate xupdate) {
 
-        Messenger.TelegramAction ta = new Messenger.TelegramAction();
-        ta.setAction(Messenger.TelegramAction.Action.SEND_MESSAGE);
-        ta.setChatId(xupdate.getSenderId());
+        Response response = new Response(xupdate.getSenderTelegramUser());
 
         switch (super.step) {
 
-            case 0:
+            case -1:
+                response.setText("Ingrese Nombre del negocio.");
+                response.execute();
+                next();
 
-                ta.setText("Ingrese id de telegram");
-                ta.execute();
+                break;
+
+            case 0:
+                name = xupdate.getText();
+                response.setText("Ingrese telefono del Negocio.");
+                response.execute();
                 next();
 
                 break;
 
             case 1:
+                phone = xupdate.getText();
+                response.setText("Ingrese direccion o ubicacion del Negocio."
+                        + "Si es posible tomarla de Google.");
 
-                telegramId = xupdate.getText();
-                
-                        /*revisamos si ya existe el TelegramUser**/
-                TelegramUser telegramUserById = DataBase.TelegramUsers.getTelegramUserById(telegramId);
-                if (telegramUserById == null) {
-
-                    ta.setText("⚠️ Este Usuario " + telegramId + " de telegram no exite, el usuario debe mandar mensaje primero a un nodo o robot official,"
-                            + " para continuar el registro. si ya lo hizo presione ingrese el Id de nuevo o de lo contrario"
-                            + " /salir");
-                    ta.setMenu(MessageMenu.ExitButton());
-                    ta.execute();
-                    break;
-                }
-                
-                
-                
-                ta.setText("Ingrese nombre del Negocio.");
-                ta.execute();
+                response.execute();
                 next();
 
                 break;
 
             case 2:
-                name = xupdate.getText();
-                ta.setText("Ingrese telefono del Negocio.");
-                ta.execute();
+
+                if (xupdate.hasLocation()) {
+                    bussines.setLocation(xupdate.getLocation().toString());
+                } else {
+                    address = xupdate.getText();
+                }
+
+                response.setText("Seleccione Area de operacion.");
+                response.setMenu(Methods.getAreas());
+                response.execute();
                 next();
 
                 break;
 
             case 3:
-                phone = xupdate.getText();
-                ta.setText("Ingrese direccion o ubicacion del Negocio.");
-                ta.execute();
+                area = xupdate.getText();
+
+                bussines = new Bussines(name, phone, address, area);
+
+                response.setText("Es correcta la informacion?\n" + bussines.toString());
+                response.setMenu(MessageMenu.yesNo());
+                response.execute();
                 next();
 
                 break;
 
             case 4:
-                
-                if (xupdate.hasLocation()) {
-                    address = xupdate.getLocation().getAddress();
-                } else {
-                    address = xupdate.getText();
-                }
-                
-                
-                ta.setText("Seleccione Area de operacion.");
-                ta.setMenu(getAreas());
-                ta.execute();
-                next();
-
-                break;
-
-            case 5:
-                area = xupdate.getText();
-
-                bussines = new Bussines(name, phone, address, area);
-
-                ta.setText("Es correcta la informacion?\n" + bussines.toString());
-                ta.setMenu(MessageMenu.yesNo());
-                ta.execute();
-                next();
-
-                break;
-
-            case 6:
-                
-           
-            
-            
 
                 if (Utils.isPositiveAnswer(xupdate.getText())) {
-                    
-                    
 
-                        DataBase.Bussiness.insertNew(bussines);
-                        DataBase.TelegramUsers.updateTelegramUserField(telegramId, "accountType", TelegramUser.AccountType.BUSSINES);
-                        DataBase.TelegramUsers.updateTelegramUserField(telegramId, "accountId", bussines.getAccountId());
+                    try {
+                        TelegramUser tu = xupdate.getSenderTelegramUser();
 
-                        DataBase.BalancesAccounts.insertNewBalanceAccount(bussines.getBalanceAccountNumber(), bussines.getAccountId());
+                        tu.setAccountType(AccountType.BUSSINES);
+                        tu.setAccountId(bussines.getAccountId());
 
-                        ta.setText("Registro completo.");
-                        ta.setMenu(MessageMenu.okAndDeleteMessage());
-                        ta.execute();
+                        DataBase.Accounts.TelegramUsers().update(tu);
+
+                        BalanceAccount balanceAccount = new BalanceAccount();
+
+                        DataBase.Contability.BalancesAccounts.BalancesAccounts().create(balanceAccount);
+                        bussines.setBalanceAccountNumber(balanceAccount.getAccountNumber());
+                        bussines.setAccountStatus(AccountStatus.PAUSED);
+                        DataBase.Accounts.Bussiness.Bussiness().create(bussines);
+
+                        response.setText("Registro completo.");
+                        response.setMenu(MessageMenu.okAndDeleteMessage());
+                        response.execute();
                         destroy();
-                    
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        response.setText(e.getLocalizedMessage());
+                        response.setMenu(MessageMenu.okAndDeleteMessage());
+                        response.execute();
+
+                    }
 
                 } else {
 
                     destroy();
-                    ta.setText("Proceso cancelado.");
-                    ta.execute();
+                    response.setText("Proceso cancelado.");
+                    response.execute();
                 }
 
                 break;
 
-
         }
-        
-      
-    }
-    
-     MessageMenu getAreas() {
-        MessageMenu menu = new MessageMenu();
-        ArrayList<OperationAreaMagnament> oamList = OperationsMagnament.getOamList();
-        for (OperationAreaMagnament o : oamList) {
-            menu.addButton(new Button(o.getName(), o.getId()), true);
-        }
-
-        return menu;
 
     }
 
